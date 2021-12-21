5d1f381e50f34fadc614e3d45a297e8a62bdd273
Legrain Samuel <samuel.legrain@bstorm.be>
2021-12-21 13:49:13 +0100

    Add Demo Pipe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HelloWorldComponent } from './components/hello-world/hello-world.component';
+import { PipeDemoComponent } from './components/pipe-demo/pipe-demo.component';
 
 const routes: Routes = [
-  {path:"demo_01", component : HelloWorldComponent}
+  {path:"demo_01", component : HelloWorldComponent},
+  {path:"demo_02", component : PipeDemoComponent}
 ];
 
 @NgModule({
